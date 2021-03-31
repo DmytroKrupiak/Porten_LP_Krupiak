09a0cafde14d5f920b2ac1de831b7bc44b402aae
Dmytro Krupiak <mrdemonast@gmail.com>
2021-03-31 12:11:38 +0300

    changed subscribe_btn in low resolution, added keydown on slider, added small paddings to collection_description

--- script.js
+++ script.js
@@ -4,13 +4,18 @@ const burger = document.querySelector('.nav_burger'),
       carousels = document.querySelectorAll('.carousel');
 
 
+
 burger.addEventListener('click',()=>{
     burger.classList.toggle('active');
     menu.classList.toggle('active');
     burger.classList.contains('active')
-    ? carousels.forEach(i=>i.style.visibility='hidden')
-    : carousels.forEach(i=>i.style.visibility='visible')
+    ? carousels.forEach(i=>{i.style.opacity='0';
+                            i.style.transition = 'opacity 0.5s ease-in'})
+    : carousels.forEach(i=>i.style.opacity='1')
 });
+
+
+
 
 
 
